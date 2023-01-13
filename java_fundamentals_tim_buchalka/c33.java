39a0c949b5c8050af77f94268a793e0d67891fa4
Martin Rombach <martinrombach88@gmail.com>
2023-01-13 11:14:27 +0900

    33

---
+++ java_fundamentals_tim_buchalka/c33.java
@@ -0,0 +1,64 @@
+public class c33 {
+    //     Flour Pack Problem
+    // Write a method named canPack with three parameters of type int named bigCount, smallCount, and goal. 
+
+    // The parameter bigCount represents the count of big flour bags (5 kilos each).
+
+    // The parameter smallCount represents the count of small flour bags (1 kilo each).
+
+    // The parameter goal represents the goal amount of kilos of flour needed to assemble a package.
+
+    // Therefore, the sum of the kilos of bigCount and smallCount must be at least equal to the value of goal. The method should return true if it is possible to make a package with goal kilos of flour.
+    
+    public static void main(String[] args) {
+        // System.out.println(canPack (1, 0, 4)); 
+        // //should return false since bigCount is 1 (big bag of 5 kilos) and goal is 4 kilos.
+
+        // System.out.println(canPack (1, 0, 5)); 
+        // //should return true since bigCount is 1 (big bag of 5 kilos) and goal is 5 kilos.
+
+        // System.out.println(canPack (0, 5, 4)); 
+        // // should return true since smallCount is 5 (small bags of 1 kilo) and goal is 4 kilos, and we have 1 bag left which is ok as mentioned above.
+
+        // System.out.println(canPack (2, 2, 11)); 
+        // // should return true since bigCount is 2 (big bags 5 kilos each) and smallCount is 2 (small bags of 1 kilo), makes in total 12 kilos and goal is 11 kilos. 
+
+        // System.out.println(canPack (-3, 2, 12));
+        // // should return false since bigCount is negative.
+
+        // System.out.println(canPack (2, 1, 5)); //returns true 1 big bag covers the goal
+
+        System.out.println(canPack (26, 0, 69));
+    }
+
+    public static boolean canPack(int bigCount, int smallCount, int goal) {
+        //1. Check negative results
+        //2. Check if goal is too large
+        //3. A condition to check bigCount and smallCount additions
+        
+        if (goal < 0 || smallCount < 0 || bigCount < 0) {
+            //If you return here, the code block following doesn't affect the results.
+            return false;
+        //Anything greater than goal returns as false.
+        } else if (goal > ((bigCount * 5) + smallCount)) {
+            return false;
+        
+        //Additional bags
+        //Divide by 5 (bigCount is worth 5 so modulo cuts down to small count left.
+        
+        //Why does this handle large big counts? 
+        //With modulo, the code doesn't try to use every bigCount, it checks if 5 can go into it.
+        //by showing us the division of five and the remainder.
+        //If the goal is too big for bigCount, it's already handled.
+        //If bigCount is big and smallCount is zero
+
+        //If the remainder is bigger than small count, return false.
+        } else {
+            int remainder =  goal % 5;
+            System.out.println(remainder);
+            return (goal % 5 <= smallCount);
+
+        }
+
+    }
+}
