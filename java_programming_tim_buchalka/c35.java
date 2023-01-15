7bdbe276643000990b29cf30e98f02bf4d9267c6
Martin Rombach <martinrombach88@gmail.com>
2023-01-15 11:19:14 +0900

    35

--- java_programming_tim_buchalka/c35.java
+++ java_programming_tim_buchalka/c35.java
@@ -34,30 +34,31 @@ public class c35 {
     public static void printSquareStar(int n) {
         //If less than 5, can't build star with console
         if (n < 5) {
-            System.out.println("Invalid value.");
-        }
+            System.out.println("Invalid Value");
+        } else {
         //Make rows 
-        for (int row = 1; row < (n + 1); row ++) {
-            System.out.println("");
-            for (int col = 0; col < n; col++) {
-                if (row == 1 || row == n) {
-                    System.out.print("*");
-                } else {
-                    //This condition decides when to print star
-                    //Use breakpoints to check whether you print in the right place
-                    //col == 0 --> always print first column
-                    //col == row - 1 --> makes diagonal line down and right
-                    //col == (n - row) --> makes diagonal line down and left
-                    //col == (n-1) --> always print last column
-                    if (col == 0 || col == (row - 1) || col == (n - row) || col == (n - 1)) {
+            for (int row = 1; row < (n + 1); row ++) {
+                System.out.println("");
+                for (int col = 0; col < n; col++) {
+                    if (row == 1 || row == n) {
                         System.out.print("*");
                     } else {
-                        System.out.print(" ");
+                        //This condition decides when to print star
+                        //Use breakpoints to check whether you print in the right place
+                        //col == 0 --> always print first column
+                        //col == row - 1 --> makes diagonal line down and right
+                        //col == (n - row) --> makes diagonal line down and left
+                        //col == (n-1) --> always print last column
+                            if (col == 0 || col == (row - 1) || col == (n - row) || col == (n - 1)) {
+                                System.out.print("*");
+                            } else {
+                                System.out.print(" ");
+                            }
+                            
                     }
-                    
                 }
-            }
 
+            }
         }
     }
 }
