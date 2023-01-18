02c412c33199dec47f0f5312766c767319c6aa7e
Martin Rombach <martinrombach88@gmail.com>
2023-01-18 21:53:14 +0900

    44

---
+++ java_programming_tim_buchalka/Puzzles 41-50/c44/c44.java
@@ -0,0 +1,23 @@
+public class c44 {
+
+    public static void main(String[] args) {
+        
+// → TEST CODE:
+
+Wall wall = new Wall(5,4);
+System.out.println("area= " + wall.getArea());
+
+wall.setHeight(-1.5);
+System.out.println("width= " + wall.getWidth());
+System.out.println("height= " + wall.getHeight());
+System.out.println("area= " + wall.getArea());
+
+
+// → OUTPUT:
+
+// area= 20.0
+// width= 5.0
+// height= 0.0
+// area= 0.0
+    }
+}
