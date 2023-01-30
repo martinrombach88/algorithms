3120d44391e092251847e18294427f5fce7474bf
Martin Rombach <martinrombach88@gmail.com>
2023-01-30 13:54:28 +0900

    49

--- java_programming_tim_buchalka/Puzzles 41-50/c49/Circle.java
+++ java_programming_tim_buchalka/Puzzles 41-50/c49/Circle.java
@@ -10,19 +10,15 @@ public class Circle {
 
     private double radius;
 
-    public Circle() {
-    }
-
     public Circle(double radius) {
         this.radius = (radius <= 0) ? 0 : radius;
     }
 
     public double getRadius() {
-
         return this.radius;
     }
 
-    public void getArea() {
-        this.radius = (radius * radius) * Math.PI;
+    public double getArea() {
+        return (radius * radius) * Math.PI;
     }
 }
--- java_programming_tim_buchalka/Puzzles 41-50/c49/Main.java
+++ java_programming_tim_buchalka/Puzzles 41-50/c49/Main.java
@@ -1,7 +1,33 @@
 public class Main {
     // Cylinder
-    public static void main(String[] args) {
+    // 1. Write a class with the name Circle. The class needs one field (instance
+    // variable) with name radius of type double.
 
+    // The class needs to have one constructor with parameter radius of type double
+    // and it needs to initialize the fields.
+
+    // In case the radius parameter is less than 0 it needs to set the radius field
+    // value to 0.
+
+    // 2. Write a class with the name Cylinder that extends Circle class. The class
+    // needs one field (instance variable) with name height of type double.
+
+    // The class needs to have one constructor with two parameters radius and height
+    // both of type double. It needs to call parent constructor and initialize a
+    // radius field.
+
+    // In case the height parameter is less than 0 it needs to set the height field
+    // value to 0.
+
+    public static void main(String[] args) {
+        Circle circle = new Circle(3.75); // circle.radius= 3.75
+        System.out.println("circle.radius= " + circle.getRadius()); // circle.area= 44.178646691106465
+        System.out.println("circle.area= " + circle.getArea());
+        Cylinder cylinder = new Cylinder(5.55, 7.25);
+        System.out.println("cylinder.radius= " + cylinder.getRadius()); // cylinder.radius= 5.55
+        System.out.println("cylinder.height= " + cylinder.getHeight()); // cylinder.height= 7.25
+        System.out.println("cylinder.area= " + cylinder.getArea()); // cylinder.area= 96.76890771219959
+        System.out.println("cylinder.volume= " + cylinder.getVolume()); // cylinder.volume= 701.574580913447
     }
 
 }
