ebd19a1f52bf11316ae27a996566f74a77923969
Martin Rombach <martinrombach88@gmail.com>
2023-02-02 21:34:46 +0900

    57

---
+++ java_programming_tim_buchalka/Puzzles 51-60/57/Main.java
@@ -0,0 +1,27 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        //No output to show - run the code and enter ints for results.
+        System.out.println("Minimum value: " + findMin(readIntegers()));
+    }
+
+    public static int[] readIntegers() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a series of integers separated by commas.");
+        String[] stringArray = scanner.nextLine().split(",");
+        int[] intArray = new int[stringArray.length];
+        for (int i = 0; i < stringArray.length; i++) {
+             intArray[i] = Integer.parseInt(stringArray[i]);
+        }
+        System.out.println("You made this array: " + Arrays.toString(intArray));
+        return intArray;
+    }
+
+    public static int findMin(int[] argArray) {
+        //Important -> i < argArray.length means 0 based ending. <= would give an error.
+        Arrays.sort(argArray);
+        return argArray[0];
+    }
+}
