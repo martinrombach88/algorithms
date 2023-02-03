3846e1666bf3edddd5ffc7b69fbadb77dc86b91f
Martin Rombach <martinrombach88@gmail.com>
2023-02-03 22:30:17 +0900

    58, 58 comment thread

--- java_programming_tim_buchalka/Puzzles 51-60/57/Main.java
+++ java_programming_tim_buchalka/Puzzles 51-60/57/Main.java
@@ -3,7 +3,8 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        //No output to show - run the code and enter ints for results.
+        // No output to show - run the code and enter ints for results.
+        // Validation beyond the scope of the challenge.
         System.out.println("Minimum value: " + findMin(readIntegers()));
     }
 
@@ -13,14 +14,18 @@ public class Main {
         String[] stringArray = scanner.nextLine().split(",");
         int[] intArray = new int[stringArray.length];
         for (int i = 0; i < stringArray.length; i++) {
-             intArray[i] = Integer.parseInt(stringArray[i]);
+            // intArray[i] = Integer.parseInt(stringArray[i]);
+            // Tim chained .trim() onto the stringArray. This takes care of white spaces for
+            // validation.
+            intArray[i] = Integer.parseInt(stringArray[i].trim());
         }
         System.out.println("You made this array: " + Arrays.toString(intArray));
         return intArray;
     }
 
     public static int findMin(int[] argArray) {
-        //Important -> i < argArray.length means 0 based ending. <= would give an error.
+        // Important -> i < argArray.length means 0 based ending. <= would give an
+        // error.
         Arrays.sort(argArray);
         return argArray[0];
     }
