5ac1a1a0b850d1c0ace749d4ba7ca8f78e4f24e5
Martin Rombach <martinrombach88@gmail.com>
2023-02-18 23:37:37 +0900

    67 progress

--- java_programming_tim_buchalka/Puzzles 61-70/67/Location.java
+++ java_programming_tim_buchalka/Puzzles 61-70/67/Location.java
@@ -4,16 +4,21 @@ import java.util.Map;
 public class Location {
     private final int locationID;
     private final String description;
-    private final Map<String[], Integer> exits;
+    private final Map<String, Integer> exits;
 
     public Location(int locationID, String description) {
         this.locationID = locationID;
         this.description = description;
-        this.exits = new HashMap<String[], Integer>();
-        this.exits.put(new String[]{"Q", "Quit Game", "Quit", "I need to Quit"}, 0);
+        this.exits = new HashMap<String, Integer>();
+
+
+        //Here you can definitely change Q to QUIT and it works. However, you need to make it receive multiple inputs.
+        //Inputs should be QUIT, and accept the split Q as well as Concat strings I want to Quit/Quit Game
+        this.exits.put("QUIT", 0);
     }
-    public void addExit(String[] directions, int location) {
+    public void addExit(String directions, int location) {
         exits.put(directions,location);
+//        System.out.println(exits);
     }
     public int getLocationID() {
         return locationID;
@@ -23,7 +28,7 @@ public class Location {
         return description;
     }
 
-    public Map<String[], Integer> getExits() {
-        return new HashMap<String[], Integer>(exits);
+    public Map<String, Integer> getExits() {
+        return new HashMap<String, Integer>(exits);
     }
 }
--- java_programming_tim_buchalka/Puzzles 61-70/67/Main.java
+++ java_programming_tim_buchalka/Puzzles 61-70/67/Main.java
@@ -1,6 +1,4 @@
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
 //    Implement the command() method in the Main class to allow players to type full words, or phrases, then move to the correct location based upon their input.
@@ -9,67 +7,102 @@ public class Main {
 
 
     private static Map<Integer, Location> locations = new HashMap<Integer, Location>();
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        //CHANGE - Refactor of Strings to loop in, and refactor of exits to include new input strings.
+        //Refactor of Strings to loop and put, and refactor of exits to include new input strings.
         String[] locationStrings = {"You are sitting in front of a computer learning Java", "You are standing at the end of a road before a small brick building", "You are at the top of a hill", "You are inside a building, a well house for a small spring", "You are in a valley beside a stream", "You are in the forest"};
-
-        //These are identical, so really your arrays should contain North, South, East, West
-        //and split out the first letter from it
+        String[] exitNames = {"WEST","EAST", "SOUTH", "NORTH", "W", "E", "S", "N"};
+        //Array should be North, South, East, West
+        //and split out the first letter from it in the if block/switch
         //You can accept Go/Run/Walk + Direction as an input and convert
-
-        String[] northExits = {"N", "North", "Go North", "Run North", "Walk North"};
-        String[] southExits = {"S", "South", "Go South", "Run South", "Walk South"};
-        String[] eastExits = {"E", "East", "Go East", "Run East", "Walk East"};
-        String[] westExits = {"W", "West", "Go West", "Run West", "Walk West"};
 
         for (int i = 0; i < locationStrings.length; i++) {
             locations.put(i, new Location(i, locationStrings[i]));
         }
+        //North, N
+        locations.get(1).addExit(exitNames[3], 2);
+        locations.get(1).addExit(exitNames[7], 2);
+        //South, S
+        locations.get(1).addExit(exitNames[2], 3);
+        locations.get(1).addExit(exitNames[6], 3);
 
-        locations.get(1).addExit(westExits, 2);
-        locations.get(1).addExit(eastExits, 3);
-        locations.get(1).addExit(southExits, 4);
-        locations.get(1).addExit(northExits, 5);
-        locations.get(2).addExit(northExits, 5);
-        locations.get(3).addExit(westExits, 1);
-        locations.get(4).addExit(northExits, 1);
-        locations.get(4).addExit(westExits, 2);
-        locations.get(5).addExit(southExits, 1);
-        locations.get(5).addExit(westExits, 2);
+        //East, E
+        locations.get(1).addExit(exitNames[1], 4);
+        locations.get(1).addExit(exitNames[5], 4);
 
+        //West, W
+        locations.get(1).addExit(exitNames[0], 5);
+        locations.get(1).addExit(exitNames[4], 5);
+
+        locations.get(2).addExit(exitNames[0], 5);
+        locations.get(2).addExit(exitNames[4], 5);
+        locations.get(3).addExit(exitNames[3], 1);
+        locations.get(3).addExit(exitNames[7], 1);
+        locations.get(4).addExit(exitNames[0], 1);
+        locations.get(4).addExit(exitNames[4], 1);
+        locations.get(4).addExit(exitNames[3], 2);
+        locations.get(4).addExit(exitNames[7], 2);
+
+        locations.get(5).addExit(exitNames[1], 1);
+        locations.get(5).addExit(exitNames[5], 1);
+
+        locations.get(5).addExit(exitNames[3], 2);
+        locations.get(5).addExit(exitNames[7], 2);
+
+        //PSEUDO
+        //Contain the scanner and while loop in the command method
+
+//        public static void command() {
         int loc = 1;
-        while(true) {
+        while (true) {
+            boolean found = false;
             System.out.println(locations.get(loc).getDescription());
-            if(loc == 0) {
+            if (loc == 0) {
                 break;
             }
-            //Change String to String[]
-            Map<String[], Integer> exits = locations.get(loc).getExits();
-            System.out.println("Available exits are ");
 
-            //Print exit[0] instead of just the exit key
-            for(String[] exit : exits.keySet()) {
-                System.out.print(exit[0] + " , ");
+            Map<String, Integer> exits = locations.get(loc).getExits();
+            System.out.print("Available exits are ");
+            for (String exit : exits.keySet()) {
+                if (exit.length() > 1) {
+                    System.out.print(exit + ", ");
+                }
+
             }
             System.out.println();
 
-            //This line takes user input, if block checks it
             String direction = scanner.nextLine().toUpperCase();
 
-            //PSEUDO
-            //Loop over an exit key's array. If array contains the user input string,
-            //loc = exits[arrayKey[i][0]]
-            //-> can you select this? Do you need a different structure for these keys?
-
-            if(exits.containsKey(direction)) {
-                loc = exits.get(direction);
+            for (String exit : exits.keySet()) {
+                if (direction.contains(exit)) {
+                    loc = exits.get(exit);
+                    found = true;
+                    break;
+                }
+            }
+            if (!found) {
+                System.out.println("You cannot go in that direction");
             } else {
-                System.out.println("You cannot go in that direction.");
+                found = false;
             }
 
+
         }
+    }
+}
+/*                if (exits.containsKey(direction)) {
+                    loc = exits.get(direction);
+            } else {
+            System.out.println("You cannot go in that direction");*/
+
+
+
+
+
+
+
 
 //        public static void command() {
 //
@@ -84,8 +117,3 @@ public class Main {
 //        String[] building = "You are inside a building, a well house for a small spring".split(", ");
 //        for (String i : building) {
 //            System.out.println(i);
-//        }
-    }
-
-
-}
