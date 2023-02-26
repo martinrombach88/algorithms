66f14eb60575c2b890ea631d07b71c36e0a76eda
Martin Rombach <martinrombach88@gmail.com>
2023-02-26 09:26:26 +0900

    12

---
+++ jetbrains/11-20/12/Main.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+class Main {
+    //Goal: If n and m are NEVER next to one another in the user input array, return true.
+
+    //Example 
+    // 10
+    // 3 4 5 6 3 4 6 4 4 8
+    // 5 8 -> 5 and 8 are not next to each other so returns true
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int length = Integer.parseInt(scanner.nextLine());
+        String[] elems = scanner.nextLine().split(" ");
+        String[] nAndM = scanner.nextLine().split(" ");
+        int n = Integer.parseInt(nAndM[0]);
+        int m = Integer.parseInt(nAndM[1]);
+        boolean neverNextTo = true;
+
+        for (int i = 0; i < length - 1; i++) {
+            if(Integer.parseInt(elems[i]) == n) {
+                if (Integer.parseInt(elems[i + 1]) == m) {
+                    neverNextTo = false;
+                }
+            } else if (Integer.parseInt(elems[i]) == m) {
+                if (Integer.parseInt(elems[i + 1]) == n) {
+                    neverNextTo = false;
+                }
+            }
+
+        }
+        System.out.println(neverNextTo);
+    }
+}
