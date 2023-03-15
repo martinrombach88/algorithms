8088bde8c2fcc03d2ed6bd541d2c511ff0509c44
Martin Rombach <martinrombach88@gmail.com>
2023-03-15 10:57:14 +0900

    40

--- jetbrains/31-40/40/Main.java
+++ jetbrains/31-40/40/Main.java
@@ -1,3 +1,4 @@
+
 /*
  * 
 Vehicle registration number
@@ -16,18 +17,47 @@ Sample Output 1:
 
 true
 
+AB123C
+
+
 key point - don't rely on jetbrains for tests. instead, anticipate possible tests and make your own
+
+Checking the length -> If you have a set length, check too long + too short (length != target)
 
  */
 import java.util.Scanner;
 
 class Main {
     public static void main(String[] args) {
+        String regexLatin = "[ABEKMHOPCTYX]";
+        String regexInt = "[0-9]";
         Scanner scanner = new Scanner(System.in);
-
-        String regNum = scanner.nextLine(); // a valid or invalid registration number
-
-        /* write your code here */
+        String[] regNum = scanner.nextLine().split(""); // a valid or invalid registration number
+        boolean result = true;
+        Boolean[] checks = { false, false, false, false, false, false };
+        for (int i = 0; i < regNum.length; i++) {
+            if (regNum.length != 6) {
+                result = false;
+                break;
+            } else {
+                if (i == 0 || i == 4 || i == 5) {
+                    // System.out.println("i:" + regNum[i] + " latin:" +
+                    // regNum[i].matches(regexLatin));
+                    checks[i] = regNum[i].matches(regexLatin) ? true : false;
+                } else if (i == 1 || i == 2 || i == 3) {
+                    // System.out.println("i:" + regNum[i] + " int:" +
+                    // regNum[i].matches(regexInt));
+                    checks[i] = regNum[i].matches(regexInt) ? true : false;
+                }
+            }
+        }
+        for (boolean check : checks) {
+            if (check == false) {
+                result = false;
+                break;
+            }
+        }
+        System.out.println(result);
     }
 
 }
