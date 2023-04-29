b2a4b488f6501db159b41e79217429b914f66107
martinrombach88 <martinrombach88@gmail.com>
2023-04-29 23:47:45 +0900

    1 - Pagination Helper progress

--- js/codewars/1-10/1/index.js
+++ js/codewars/1-10/1/index.js
@@ -2,45 +2,43 @@ class PaginationHelper {
   constructor(collection, itemsPerPage) {
     this.collection = collection;
     this.itemsPerPage = itemsPerPage;
+    this.pCount = this.pageCount();
     // The constructor takes in an array of items and a integer indicating how many
     // items fit within a single page
+    this.paginArray = [];
   }
   itemCount() {
     return this.collection.length;
     // returns the number of items within the entire collection
   }
-  pageCount(collection, itemsPerPage) {
+  pageCount() {
     return Math.round(this.collection.length / this.itemsPerPage);
     // returns the number of pages
   }
   pageItemCount(pageIndex) {
-    //if pageIndex doesn't exist, return -1
-    console.log(this);
-    let paginArray = [];
-    if (pageIndex > this.pageCount()) {
-      return -1;
-    } else {
-      while (this.collection.length != 0) {
-        paginArray.push(this.collection.splice(0, this.itemsPerPage));
-      }
-      // return paginArray[pageIndex].length;
-      return paginArray;
-
-      //helper -> 0: 4, 1: 2
-      //pagArray = [[item1, item2, item3, item4], [item5, item6]]
-      //then return pagArray[index].length
-      //push the items into pagination, objects that represent the pagination?
-      //arrays in arrays are better?
-      //split an array up by the pagination?
+    const collectionCopy = this.collection;
+    while (collectionCopy.length != 0) {
+      this.paginArray.push(collectionCopy.splice(0, this.itemsPerPage));
     }
-
+    if (this.paginArray[pageIndex]) {
+      return this.paginArray[pageIndex].length;
+    } else {
+      return -1;
+    }
     // returns the number of items on the current page. page_index is zero based.
     // this method should return -1 for pageIndex values that are out of range
   }
+
   pageIndex(itemIndex) {
+    const collectionCopy = this.collection;
+    if ((this.paginArray.length = 0)) {
+      while (collectionCopy.length != 0) {
+        this.paginArray.push(collectionCopy.splice(0, this.itemsPerPage));
+      }
+    }
+
     // determines what page an item is on. Zero based indexes
     // this method should return -1 for itemIndex values that are out of range
-    //use findIndex
   }
 }
 
@@ -48,12 +46,11 @@ const example = new PaginationHelper([1, 2, 3, 4, 5, 6], 4);
 // example.itemCount();
 
 var helper = new PaginationHelper(["a", "b", "c", "d", "e", "f"], 4);
-console.log(helper.pageCount());
-//should == 2
-helper.itemCount(); //should == 6
-console.log(helper.pageItemCount(0)); //should == 4
-console.log(helper.pageItemCount(1)); // last page - should == 2
-console.log(helper.pageItemCount(2)); // should == -1 since the page is invalid
+console.log(helper.pageCount(), "should ==2"); //should == 2
+console.log(helper.itemCount(), "should == 6");
+console.log(helper.pageItemCount(0), "should ==4"); //should == 4
+console.log(helper.pageItemCount(1), "should ==2"); // last page - should == 2
+console.log(helper.pageItemCount(2), "should ==-1"); // should == -1 since the page is invalid
 
 // var helper2 = new PaginationHelper(
 //   ["a", "b", "c", "d", "e", "f", "a", "b", "c", "d", "e", "f"],
