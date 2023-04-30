869d71631bcda4fc873e264f29c0f8f488403be5
martinrombach88 <martinrombach88@gmail.com>
2023-04-30 21:57:32 +0900

    1 - Pagination Helper

--- js/codewars/1-10/1/index.js
+++ js/codewars/1-10/1/index.js
@@ -3,16 +3,16 @@ class PaginationHelper {
     this.collection = collection;
     this.itemsPerPage = itemsPerPage;
     this.pCount = this.pageCount();
+    this.paginArray = [];
     // The constructor takes in an array of items and a integer indicating how many
     // items fit within a single page
-    this.paginArray = [];
   }
   itemCount() {
     return this.collection.length;
     // returns the number of items within the entire collection
   }
   pageCount() {
-    return Math.round(this.collection.length / this.itemsPerPage);
+    return Math.ceil(this.collection.length / this.itemsPerPage);
     // returns the number of pages
   }
   pageItemCount(pageIndex) {
@@ -36,24 +36,18 @@ class PaginationHelper {
         this.paginArray.push(collectionCopy.splice(0, this.itemsPerPage));
       }
     }
-
+    const array = this.paginArray;
+    let count = 0;
+    for (let i = 0; i < array.length; i++) {
+      for (let j = 0; j < array[i].length; j++) {
+        if (count === itemIndex) {
+          return i;
+        }
+        count++;
+      }
+    }
+    return -1;
     // determines what page an item is on. Zero based indexes
     // this method should return -1 for itemIndex values that are out of range
   }
 }
-
-const example = new PaginationHelper([1, 2, 3, 4, 5, 6], 4);
-// example.itemCount();
-
-var helper = new PaginationHelper(["a", "b", "c", "d", "e", "f"], 4);
-console.log(helper.pageCount(), "should ==2"); //should == 2
-console.log(helper.itemCount(), "should ==6");
-console.log(helper.pageItemCount(0), "should ==4"); //should == 4
-console.log(helper.pageItemCount(1), "should ==2"); // last page - should == 2
-console.log(helper.pageItemCount(2), "should ==-1"); // should == -1 since the page is invalid
-
-// // pageIndex takes an item index and returns the page that it belongs on
-// helper.pageIndex(5); //should == 1 (zero based index)
-// helper.pageIndex(2); //should == 0
-// helper.pageIndex(20); //should == -1
-// helper.pageIndex(-10); //should == -1
