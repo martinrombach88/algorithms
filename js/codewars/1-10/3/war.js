42455972b70ca14c4d458dc103c9ac9b9d8e212d
martinrombach88 <martinrombach88@gmail.com>
2023-05-29 20:15:39 +0900

    refactor, external link for codewars

---
+++ js/codewars/1-10/3/war.js
@@ -0,0 +1,56 @@
+/*
+Introduction
+There is a war and nobody knows - the alphabet war!
+There are two groups of hostile letters. The tension between left side letters and right side letters was too high and the war began.
+
+Task
+Write a function that accepts fight string consists of only small letters and return who wins the fight. When the left side wins return Left side wins!, when the right side wins return Right side wins!, in other case return Let's fight again!.
+
+The left side letters and their power:
+
+ w - 4
+ p - 3
+ b - 2
+ s - 1
+The right side letters and their power:
+
+ m - 4
+ q - 3
+ d - 2
+ z - 1
+The other letters don't have power and are only victims.
+*/
+
+function alphabetWar(fight)
+{
+  let f = fight.split("")
+  const points = { "s": 'l1', "b": 'l2', "p": 'l3', "w": 'l4', "z": 'r1', "d": 'r2', "q": 'r3', "m": 'r4'}
+  let l = 0
+  let r = 0
+   for (let i = 0; i < f.length; i++) {
+     for (const key in points) {
+        if (key === f[i]) {
+            let curr = points[key].split("")
+            if (curr[0] == 'l') {
+                l += parseInt(curr[1])
+            } else if (curr[0] == "r") {
+                r += parseInt(curr[1])
+            }
+        }
+     }  
+   }
+
+  if (l > r) {
+    return "Left side wins!"
+  } else if (r > l) {
+    return "Right side wins!"
+  } else {
+   return "Let\'s fight again!";
+    }
+
+}
+
+console.log(alphabetWar("z"), "should be right wins")
+console.log( alphabetWar("zdqmwpbs") , "should be: Let's fight again!" );
+console.log( alphabetWar("zzzzs"), "should be: Right side wins!" );
+console.log( alphabetWar("wwwwww"), "should be: Left side wins!" );
