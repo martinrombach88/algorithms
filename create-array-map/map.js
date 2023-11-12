53f7ae4725b097c3ee58479169a5024aeb0200c4
Martin Rombach <martinrombach88@gmail.com>
2023-11-12 23:55:02 +0900

    create-array-map

--- create-array-map/map.js
+++ create-array-map/map.js
@@ -1,12 +1,30 @@
 /*
 recreate the array map function manually
+map runs the callback on each value in an array
+and returns an array of values based on that function
+if console.log is entered, console.log doesn't
+return anything, so comes back undefined
+
 map(callbackFn) only
 */
 Array.prototype.myMap = function(func) {
+    const mapped = [];
+    // for (let val in this) {
+    //     mapped.push(func(this[val]));
+    // }
+    //why doesn't it work in for in?
     for (let i = 0; i < this.length; i++) {
-        func(this[i]);
+        mapped[i] = func(this[i]);
     }
+    // return mapped;
 }
 
-[1,2,3].myMap(console.log);
-["a","b","c"].myMap(console.log);
+const arr1 = [1,2,3].myMap(console.log);
+const arr2 = ["a","b","c"].myMap(console.log);
+const arr3 = [1,2,3].myMap((item) => item * 2);
+console.log(arr1); // array of console.log the function * 3, 
+//console.log doesn't return a value so undefined is returned
+console.log(arr2); //see above
+
+console.log(arr3); //2, 4, 6
+
