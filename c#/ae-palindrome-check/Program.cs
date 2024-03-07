1a600022438ce986f9c31540b689f60dc23e9361
Martin Rombach <martinrombach88@gmail.com>
2024-03-07 23:10:42 +0900

    palindrome check

---
+++ c#/ae-palindrome-check/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+static bool IsPalindrome(string str) {
+    // Write your code here.
+    string comp = "";
+    //do it without a reverse method?
+    //loop and create new string, compare with initial string
+    for (int i = str.Length-1; i >= 0; i--) {
+        comp = String.Concat(comp + str[i]);
+    }
+    if (comp == str) {
+        return true;
+    }
+    return false;
+  }
+
+Console.WriteLine(IsPalindrome("maxam")); //true
+Console.WriteLine(IsPalindrome("cheese")); //false
