d2b2018607d05ff6923a9dd20f0f818c778dc8bd
Martin Rombach <martinrombach88@gmail.com>
2024-04-02 23:39:23 +0900

    algo expert run length encoding init

---
+++ c#/ae-run-length-encoding/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;  
+
+
+static string RunLengthEncoding(string str) {
+int c = 0; //count
+char t = str[0]; //target
+string r = ""; 
+
+    
+for (int i = 0; i < str.Length; i++) {
+    bool target = str[i] == t;
+
+    if (target && c == 9 || !target) {
+        r = r + c + t;
+        t = str[i];
+        c = 0;
+    }
+    if (target && c < 9) {
+        c++;
+    }
+}
+//Console.WriteLine(c);
+
+return r;
+}
+
+Console.WriteLine(RunLengthEncoding("AAAAAAAAAAAAABBCCCCDD"));
+
+//expected
+//9A4A2B4C2D
+
+//currently returning 9A4A1B3C
---
+++ masters_memory_mountain/Program.cs
@@ -0,0 +1,16 @@
+﻿
+
+int test(int elems, int stride) {
+    long i, sx2 = stride*2, sx3 = stride*3;
+    long accO = 0, accl = 0, acc2 = 0, acc3;
+    long length = elems;
+    long limit = length - (stride*4);
+
+    for (i = 0; i < limit; i += sx4) {
+        accO = accO + datafi];
+        accl = accl + data[i+stride];
+        acc2 = acc2 + data[i+sx2];
+        acc3 = acc3 + data[i+sx3];
+    }
+    return 1;
+}
